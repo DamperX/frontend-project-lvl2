23c49c6299c925af469e71abe47e54dff9cdc149
Александр <gosu_1994@mail.ru>
2021-11-26 10:46:29 +0300

    add options and format to help

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,8 @@ import { program } from 'commander';
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .argument('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
