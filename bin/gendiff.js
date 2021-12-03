67a287d6d4edb853d6e3d41013592fd549be357c
Александр <gosu_1994@mail.ru>
2021-12-03 12:23:36 +0300

    complete gendiff func

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import genDiff from '../src/index.js';
 
 program
   .version('0.0.1')
-  .argument('<filepath1> <filepath2>')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format');
-
-program.parse();
+  .option('-f, --format <type>', 'output format')
+  .action(genDiff)
+  .parse();
---
+++ src/index.js
@@ -0,0 +1,78 @@
+import path from 'path';
+import process from 'process';
+import { readFileSync } from 'fs';
+import _ from 'lodash';
+
+const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
+
+const getContentFromFile = (filepath) => readFileSync(filepath, 'utf-8');
+
+const compareObjects = (firstObject, secondObject) => {
+  const keysArray = Object.keys({ ...firstObject, ...secondObject });
+
+  const result = keysArray.map((key) => {
+    if (!_.has(firstObject, key)) {
+      return {
+        key,
+        value: secondObject[key],
+        mod: '+',
+      };
+    }
+
+    if (!_.has(secondObject, key)) {
+      return {
+        key,
+        value: firstObject[key],
+        mod: '-',
+      };
+    }
+
+    if (firstObject[key] === secondObject[key]) {
+      return {
+        key,
+        value: firstObject[key],
+        mod: ' ',
+      };
+    }
+
+    return {
+      key,
+      value: {
+        before: firstObject[key],
+        after: secondObject[key],
+      },
+      mod: '+-',
+    };
+  });
+
+  return _.sortBy(result, 'key');
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const absFirstFilePath = getAbsolutePath(filepath1);
+  const absSecondFilePath = getAbsolutePath(filepath2);
+
+  const firstFileContent = getContentFromFile(absFirstFilePath);
+  const secondFileContent = getContentFromFile(absSecondFilePath);
+
+  const firstFileData = JSON.parse(firstFileContent);
+  const secondFileData = JSON.parse(secondFileContent);
+
+  const data = compareObjects(firstFileData, secondFileData);
+  const result = data.reduce((acc, item) => {
+    const { key, value, mod } = item;
+
+    if (mod === '+-') {
+      const { before, after } = value;
+      acc.push(` - ${key}: ${before}`);
+      acc.push(` + ${key}: ${after}`);
+    } else {
+      acc.push(` ${mod} ${key}: ${value}`);
+    }
+    return acc;
+  }, []).join('\n');
+
+  console.log(`{\n${result}\n}`);
+};
+
+export default genDiff;
